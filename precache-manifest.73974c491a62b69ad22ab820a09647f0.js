69ca080906a15d9d48372a34aff626c5fdb00d60
arseniyT <30984464+arseniyT@users.noreply.github.com>
2018-12-10 20:56:11 +0300

    Updates

---
+++ precache-manifest.73974c491a62b69ad22ab820a09647f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56cb1333a18b4916ce08",
+    "url": "/arseniyT/hw_reactNews/static/js/runtime~main.56cb1333.js"
+  },
+  {
+    "revision": "b0bd10d7cff1cb06a669",
+    "url": "/arseniyT/hw_reactNews/static/js/main.b0bd10d7.chunk.js"
+  },
+  {
+    "revision": "03783d836442911ca812",
+    "url": "/arseniyT/hw_reactNews/static/js/1.03783d83.chunk.js"
+  },
+  {
+    "revision": "b0bd10d7cff1cb06a669",
+    "url": "/arseniyT/hw_reactNews/static/css/main.a7f4cf79.chunk.css"
+  },
+  {
+    "revision": "3e9c45762f96b8bb9ff3d23339ec2ab4",
+    "url": "/arseniyT/hw_reactNews/index.html"
+  }
+];
